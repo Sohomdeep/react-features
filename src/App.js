a130ea71a253ccbd32f3ddba7a0193debb23b93d
Sohomdeep <sohomdeeppaul@gmail.com>
2024-06-18 23:06:15 +0530

    example of useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function haldleClick(){
+    setCounter(counter+1);
+  }
+
   return (
-    <div className="App">
+    <>
+      <center>
+        <h1>Counter : {counter} </h1>
+        <button onClick={haldleClick}>Click Me</button>
+      </center>
+      {/*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +30,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/}
+    </>
+    
   );
 }
 
