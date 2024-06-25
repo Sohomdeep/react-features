b12ab7c813022ebaebf56fbb536dbd69c4d71f21
Sohomdeep <sohomdeeppaul@gmail.com>
2024-06-25 23:10:26 +0530

    another example of chindren prop, html element pass as prop.children

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import NavBar from './assets/components/NavBar'
 import Data from './assets/components/Data'
 import Button from './assets/components/Button'
+import List from './assets/components/List'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,12 @@ function App() {
       <Data name={"Sohom" + " Deep" } address="Kolkata" />
 
       <Button>Submit</Button>
+
+      <List>
+        <li>List 1</li>
+        <li>List 2</li>
+        <li>List 3</li>
+      </List>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
